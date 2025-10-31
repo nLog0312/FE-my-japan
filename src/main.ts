29350ba814a3e567961864221cab2dc6b2381df1
nLog0312 <83167699+tranwangnong@users.noreply.github.com>
2025-10-31 23:41:49 +0900

    Enabled Speed Insights Vercel

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+injectSpeedInsights();
 inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
